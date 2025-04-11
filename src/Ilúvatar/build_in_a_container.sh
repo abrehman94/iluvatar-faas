67ab62da167ccf94dad1933f83cd272185893baf
Abdul Rehman <abrehman@iu.edu>
2025-04-11 17:42:08 -0400

    [building]     building in a rust container

---
+++ src/Ilúvatar/build_in_a_container.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+populate_script_path() {
+    SCRIPT_PATH=$(dirname $1)
+    SCRIPT_PATH=$(realpath $SCRIPT_PATH)
+}
+if [[ $0 != $BASH_SOURCE ]]; then
+    populate_script_path $BASH_SOURCE
+else
+    populate_script_path $0
+fi
+ORIGINAL_PATH=$(pwd)
+cd $SCRIPT_PATH
+
+## Build current folder in a debian image 
+if false; then 
+    docker build          \
+        --network host        \
+        -t make-runner        \
+        -f ./Dockerfile.build \
+        ./
+fi
+
+docker run                                 \
+    --rm                                       \
+    -it                                        \
+    -v "$PWD":/workspace                       \
+    -v "/home/abrehman/workspace/scx/scx":/scx \
+    -w /workspace                              \
+    --network host                             \
+    make-runner                                
+
+
+
